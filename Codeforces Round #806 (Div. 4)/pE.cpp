7b68074f5e34aa88b3aac6575aedca975706dddc
lecav <lecav30@outlook.com>
2022-07-12 12:06:54 -0500

    Template problem E

---
+++ Codeforces Round #806 (Div. 4)/pE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long LL;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+	}
+}
+
+int main()
+{
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		solve();
+	}
+	return 0;
+}
