102fd2b603830febefc7d3433eecfbc917dbcd2d
lecav <lecav30@outlook.com>
2022-07-13 21:03:34 -0500

    Time limit exceeded

--- Codeforces Round #806 (Div. 4)/pF.cpp
+++ Codeforces Round #806 (Div. 4)/pF.cpp
@@ -5,32 +5,32 @@ typedef long long LL;
 
 void solve()
 {
-	int n;
-	cin >> n;
-	int arr[n];
+    int n;
+    cin >> n;
+    int arr[n];
 
-	for (int i = 0; i < n; ++i)
-		cin >> arr[i];
-	vector<pair<int, int>> vp;
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+    vector<pair<int, int>> vp;
 
-	for (int i = 0; i < n - 1; ++i)
-	{
-		for (int j = i + 1; j < n; ++j)
-		{
-			if (arr[i] < (i + 1) && (i + 1) < arr[j] && arr[j] < (j + 1))
-				vp.push_back({arr[i], arr[j]});
-		}
-	}
-	cout << vp.size() << endl;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[i] < (i + 1) && (i + 1) < arr[j] && arr[j] < (j + 1))
+                vp.push_back({arr[i], arr[j]});
+        }
+    }
+    cout << vp.size() << endl;
 }
 
 int main()
 {
-	int tc;
-	cin >> tc;
-	while (tc--)
-	{
-		solve();
-	}
-	return 0;
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
 }
