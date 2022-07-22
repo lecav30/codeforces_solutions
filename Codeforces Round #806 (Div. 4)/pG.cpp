85438d0c7f758949b274e975e22adf13ad39ea09
lecav <lecav30@outlook.com>
2022-07-22 13:49:51 -0500

    Advanced problem

--- Codeforces Round #806 (Div. 4)/pG.cpp
+++ Codeforces Round #806 (Div. 4)/pG.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl "\n"
-typedef long long LL;
+#define ll long long
 
 void solve()
 {
@@ -9,105 +9,41 @@ void solve()
     cin >> n >> k;
     int arr[n];
 
-    LL total = 0;
+    ll total = 0;
     for (int i = 0; i < n; ++i)
     {
         cin >> arr[i];
         total += arr[i];
     }
 
-    LL coins = 0;
-
-    /* auto reduce = [&] (int i) -> void { */
-    /* }; */
-
-    /* for (int i = 0; i < n; ++i) */
-    /* { */
-    /*     // bad key */
-    /*     if (total <= k) */
-    /*     { */
-    /*         bool minusOne = false; */
-    /*         if (total % 2 == 0) */
-    /*             minusOne = true; */
-    /*         total /= 2; */
-    /*         if (minusOne) */
-    /*             total--; */
-    /*         arr[i] /= 2; */
-    /*         coins += arr[i]; */
-    /*         total -= arr[i]; */
-    /*     } */
-    /*     else { */
-    /*         // good key */
-    /*         if (arr[i] >= k) */
-    /*         { */
-    /*             LL tempTotal = total; */
-    /*             bool minusOne = false; */
-    /*             if (tempTotal % 2 == 0) */
-    /*                 minusOne = true; */
-    /*             tempTotal /= 2; */
-    /*             if (minusOne) */
-    /*                 tempTotal--; */
-    /*             if (arr[i] - k >= tempTotal / 2) */
-    /*             { */
-    /*                 coins -= k; */
-    /*                 coins += arr[i]; */
-    /*                 total -= arr[i]; */
-    /*             } */
-    /*             else { */
-    /*                 bool minusOne = false; */
-    /*                 if (total % 2 == 0) */
-    /*                     minusOne = true; */
-    /*                 total /= 2; */
-    /*                 if (minusOne) */
-    /*                     total--; */
-    /*                 arr[i] /= 2; */
-    /*                 coins += arr[i]; */
-    /*                 total -= arr[i]; */
-    /*             } */
-    /*         } */
-    /*         else  */
-    /*         { */
-    /*             // good key */
-    /*             if (total >= k * (n - i)) */
-    /*             { */
-    /*                 coins -= k; */
-    /*                 coins += arr[i]; */
-    /*                 total -= arr[i]; */
-    /*             } */
-    /*             // bad key */
-    /*             else  */
-    /*             { */
-    /*                 total /= 2; */
-    /*                 if (total % 2 == 0) */
-    /*                     total--; */
-    /*                 arr[i] /= 2; */
-    /*                 coins += arr[i]; */
-    /*                 total -= arr[i]; */
-    /*             } */
-    /*         } */
-    /*     } */
-    /*     /* cout << coins << endl; */ */
-    /* } */
+    ll coins = 0;
 
     for (int i = 0; i < n; ++i)
     {
-        bool gk = false;
-        if (gk) {
+        total -= arr[i];
+        cout << "total " << total << endl;
+        // gk
+        ll tgk = total + arr[i] - k;
+        // bk
+        ll tbk = total;
+        bool minusOne = false;
+        if (tbk % 2 == 0 && tbk != 0)
+            minusOne = true;
+        tbk /= 2;
+        if (minusOne)
+            tbk--;
+        tbk += arr[i] / 2;
+        // compare keys
+        cout << "tgk " << tgk << " - "
+             << "tbk " << tbk << endl;
+        if (tgk >= tbk)
+        {
             coins -= k;
             coins += arr[i];
-            total -= arr[i];
         }
-        else 
+        else
         {
-            bool minusOne = false;
-            if (total % 2 == 0)
-                minusOne = true;
-            total /= 2;
-            if (minusOne)
-                total--;
-            arr[i] /= 2;
-            coins += arr[i];
-            total -= arr[i];
+            coins += arr[i] /= 2;
         }
     }
 
